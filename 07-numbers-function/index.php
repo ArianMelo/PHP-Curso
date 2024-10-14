636e89146de8ac44f91e350051e538722d38d71d
Arian Melo <arianmelo7733@gmail.com>
2024-10-14 12:07:44 -0300

    funções de numeros

---
+++ 07-numbers-function/index.php
@@ -0,0 +1,15 @@
+<?php 
+
+/*
+  number_format
+  round
+  ceil
+  floor
+  rand
+*/
+
+$db = 1234.56;
+$preco = number_format($db, 2, ",", ".");
+echo "O valor do produto é R$ $preco";
+
+echo "<hr>";
